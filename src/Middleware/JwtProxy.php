3fd6ee1dc31d859464121014fdb1f02faa1b9b02
Thomas Alrek <thomas@alrek.no>
2020-03-09 18:37:21 +0100

    Implement editor proxy route

---
+++ src/Middleware/JwtProxy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Netflex\Pages\Middleware;
+
+use Closure;
+
+use Netflex\Support\JWT;
+use Netflex\Foundation\Variable;
+
+class JwtProxy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->authenticate($request);
+
+        return $next($request);
+    }
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request)
+    {
+        if ($token = $request->get('token')) {
+            $payload = JWT::decode($token)->payload;
+            /* $payload = JWT::decodeAndVerify($token, Variable::get('netflex_api')); */
+            $request->offsetUnset('token');
+            $request->offsetSet('payload', $payload);
+            return;
+        }
+
+        $this->unauthenticated($request);
+    }
+
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function unauthenticated($request)
+    {
+        abort(401);
+    }
+
+}
--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -12,8 +12,10 @@ use Netflex\Foundation\Redirect;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Collection;
+use Netflex\Pages\Middleware\JwtProxy;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -48,6 +50,7 @@ class RouteServiceProvider extends ServiceProvider
     {
       $router = $this->app->make('router');
 
+      $router->aliasMiddleware('jwt_proxy', JwtProxy::class);
       $router->aliasMiddleware('bind_page', BindPage::class);
       $router->aliasMiddleware('group_auth', GroupAuthentication::class);
 
@@ -116,6 +119,23 @@ class RouteServiceProvider extends ServiceProvider
 
     protected function mapNetflexRoutes()
     {
+      Route::middleware('jwt_proxy')
+        ->group(function () {
+
+          Route::any('.well-known/netflex', function (Request $request) {
+            $payload = $request->get('payload');
+            current_mode($payload->mode);
+            current_page($payload->page_id);
+            editor_tools($payload->edit_tools);
+
+            if ($payload->relation === 'page') {
+              return Page::findOrFail($payload->page_id)
+                ->loadRevision($payload->revision_id)
+                ->toResponse($request);
+            }
+          });
+        });
+
       Route::middleware('netflex')
         ->group(function () {
           Page::all()->filter(function ($page) {
