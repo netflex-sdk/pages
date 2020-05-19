2e4187eb031eb9dfcc4b3e763f7879f43f4e893d
Thomas Alrek <thomas@alrek.no>
2020-05-20 00:12:35 +0200

    Add support for slot in x-background-image

--- src/views/background-image.blade.php
+++ src/views/background-image.blade.php
@@ -1,29 +1,37 @@
-@if($attributes->get('class'))
-<style>
-  .{{ $attributes->get('class') }} {
-    background-image: url({!! $defaultSrc !!});
-  }
+@php($hasSlot = !!((string) $slot))
+@php($class = $attributes->get('class') ?? ($hasSlot ? ('bg_' . uniqid()) : null))
 
-  @foreach ($srcSets as $srcSet)
-    @isset($srcSet['sources']['1x'])
-      @media (max-width: {{ $srcSet['maxWidth'] }}px) {
-        .{{ $attributes->get('class') }} {
-          background-image: url({!! $srcSet['sources']['1x'] !!});
-        }
-      }
-    @endisset
-  @endforeach
+@if($class)
+  <style>
+    .{{ $class }} {
+      background-image: url({!! $defaultSrc !!});
+    }
 
-  @foreach ($srcSets as $srcSet)
-    @foreach($srcSet['sources'] as $resolution => $src)
-      @if($resolution !== '1x')
-        @media (min-resolution: {{ intval($resolution) }}dppx), and (max-width: {{ $srcSet['maxWidth'] }}px) {
-          .{{ $attributes->get('class') }} {
-            background-image: url({!! $src !!});
+    @foreach ($srcSets as $srcSet)
+      @isset($srcSet['sources']['1x'])
+        @media (max-width: {{ $srcSet['maxWidth'] }}px) {
+          .{{ $class }} {
+            background-image: url({!! $srcSet['sources']['1x'] !!});
           }
         }
-      @endif
+      @endisset
     @endforeach
-  @endforeach
-</style>
+
+    @foreach ($srcSets as $srcSet)
+      @foreach($srcSet['sources'] as $resolution => $src)
+        @if($resolution !== '1x')
+          @media (min-resolution: {{ intval($resolution) }}dppx), and (max-width: {{ $srcSet['maxWidth'] }}px) {
+            .{{ $class }} {
+              background-image: url({!! $src !!});
+            }
+          }
+        @endif
+      @endforeach
+    @endforeach
+  </style>
+  @if($hasSlot)
+    <div class="{!! $class !!}">
+      {{ $slot }}
+    </div>
+  @endif
 @endif
