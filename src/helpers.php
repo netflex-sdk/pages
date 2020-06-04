netflex-sdk/pages

commit b67ac49095467433d6e6f0a1050bd277859b3511

Only cast editor fields to HtmlString

src/helpers.php
@@ -35,7 +35,7 @@ if (!function_exists('render_component_tag')) {
     if (!Str::startsWith($component, 'x-')) {
       $component = 'x-' . $component;
     }
-    
+
     $component = str_replace('/', '.', $component);
 
     // Normalize to array in case of ComponentAttributeBag
@@ -364,17 +364,22 @@ if (!function_exists('map_content')) {
             'file' => (int) $item->file
           ]];
         });
-      case 'editor_small':
-      case 'editor_large':
-      case 'textarea':
+      case 'editor-small':
+      case 'editor-large':
         if ($item = $content->shift()) {
           return new HtmlString($item->html ?? '');
         }
 
         return null;
+      case 'textarea':
+        if ($item = $content->shift()) {
+          return $item->html ?? null;
+        }
+
+        return null;
       case 'text':
         if ($item = $content->shift()) {
-          return new HtmlString($item->text ?? '');
+          return $item->html ?? null;
         }
 
         return null;

commit aec80fdd54a6910d0723422a727e34f5152b5cb1

Fixes text content fetched from wrong column
b67ac49 introduced a bug where page content of type text would get it's value from the html column rather than text column.
This reverts the column name, while leaving the original intent of the commit.

src/helpers.php
@@ -385,7 +385,7 @@ if (!function_exists('map_content')) {
         return null;
       case 'text':
         if ($item = $content->shift()) {
-          return $item->html ?? null;
+          return $item->text ?? null;
         }
 
         return null;