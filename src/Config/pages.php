f453bf8bd4e069f6daf43dc7d6d69e4425cccf23
Thomas Alrek <thomas@alrek.no>
2020-09-10 16:49:24 +0200

    Add ability to change base controller for Netflex Pages

---
+++ src/Config/pages.php
@@ -0,0 +1,18 @@
+<?php
+
+use Netflex\Pages\Controllers\PageController;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for Netflex Pages
+    |
+    */
+    
+    'controller' => PageController::class,
+
+];
