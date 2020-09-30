c14de061c2814fd300bf8cdfd9dd515762aabe63
Thomas Alrek <thomas@alrek.no>
2020-09-30 14:58:05 +0200

    Fix XML tags not rendering correctly in certain environments

--- src/views/sitemap-xml.blade.php
+++ src/views/sitemap-xml.blade.php
@@ -1,5 +1,5 @@
-{!! '<?xml version="1.0" encoding="utf-8"?>' !!}
-{!! '<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>' !!}
+<{!! '?xml version="1.0" encoding="utf-8"?' !!}>
+<{!! '?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?' !!}>
 <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 @foreach ($entries as $entry)
