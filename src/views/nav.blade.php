0c0c5ecbd38c7614111d921efe425645d680dd8d
Thomas Alrek <thomas@alrek.no>
2020-10-28 11:35:36 +0100

    Add additional properties for nav customization

--- src/views/nav.blade.php
+++ src/views/nav.blade.php
@@ -1,22 +1,25 @@
-<ul role="menu" {{ $attributes }}>
-  @foreach ($children as $child)
-    <li>
-      <a
-        target="{{ $child->target }}"
-        href="{{ $child->url }}"
-        role="menuitem"
-      >
-        {{ $child->title }}
-      </a>
-      @if($child->children->count())
-        <x-nav
-          :class="trim($attributes->get('class') . ' dropdown-container')"
-          :parent="$child->id"
-          :levels="$levels - 1"
-          :type="$type"
-          :root="$root"
-        />
-      @endif
-    </li>
-  @endforeach
-</ul>
+@if ($levels || $levels === null)
+  <ul role="menu" {{ $attributes }}>
+    @foreach ($children as $child)
+      <li class="{{ $liClass }}">
+        <a
+          class="{{ implode(' ', array_filter([$aClass, $isActive($child) ? $activeClass : null])) }}"
+          target="{{ $child->target }}"
+          href="{{ $child->url }}"
+          role="menuitem"
+        >
+          {{ $child->title }}
+        </a>
+        @if($child->children->count())
+          <x-nav
+            :class="implode(' ', array_filter([$attributes->get('class'), $dropdownClass]))"
+            :parent="$child->id"
+            :levels="$levels !== null ? ($levels - 1) : $levels"
+            :type="$type"
+            :root="$root"
+          />
+        @endif
+      </li>
+    @endforeach
+  </ul>
+@endif
