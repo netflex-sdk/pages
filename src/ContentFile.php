9cc092ff292a9f0b4217765fad48d4ef1c9cba81
Thomas Alrek <thomas@alrek.no>
2021-02-10 11:03:10 +0100

    Add types

--- src/ContentFile.php
+++ src/ContentFile.php
@@ -5,6 +5,13 @@ namespace Netflex\Pages;
 use Netflex\Support\Accessors;
 use Netflex\Pages\Contracts\MediaUrlResolvable;
 
+/**
+ * @property int|null $id
+ * @property int|null $file
+ * @property string|null $title
+ * @property string|null $description
+ * @package Netflex\Pages
+ */
 class ContentFile implements MediaUrlResolvable
 {
     use Accessors;
@@ -18,6 +25,13 @@ class ContentFile implements MediaUrlResolvable
     {
         if ($id ?? $this->attributes['id'] ?? null) {
             return (int) $id;
+        }
+    }
+
+    public function getFileAttribute($file)
+    {
+        if ($file ?? $this->attributes['file'] ?? null) {
+            return (int) $file;
         }
     }
 
