3160b98a5bce403d2c0e84518dc03823b4ebdc55
Thomas Alrek <thomas@alrek.no>
2021-04-30 12:33:31 +0200

    Fix robots.txt template

--- resources/views/robots.blade.php
+++ resources/views/robots.blade.php
@@ -1,13 +1,14 @@
 User-Agent: *
-@if ($production)
+@if (true)
 Allow: /
-Sitemap: {{ route('sitemap.xml') }}
-@else
-Disallow: /
-@endif
 
-@foreach(config('pages.bad-bots', [] as $bot))
+@foreach(config('pages.bad-bots', []) as $bot)
 User-agent: {{ $bot }}
 Disallow: /
 
 @endforeach
+
+Sitemap: {{ route('sitemap.xml') }}
+@else
+Disallow: /
+@endif
