3156d5d483c5665207548a90553c0d9ab2df7ab5
Thomas <thomas@alrek.no>
2021-12-02 10:49:04 +0000

    prepare release

--- Providers/PagesServiceProvider.php
+++ Providers/PagesServiceProvider.php
@@ -115,16 +115,8 @@ class PagesServiceProvider extends ServiceProvider
       });
     }
 
-    Blade::directive('page', function ($name = null) {
-      $name = $name ?: '_page';
-      $name = trim($name, "\"'");
-      return "<?php echo '<input type=\"hidden\" name=\"" . $name . "\" value=\"' . (current_page() ? current_page()->id : null) . '\">'; ?>";
-    });
-
-    Blade::directive('blockhash', function ($name = null) {
-      $name = $name ?: '_blockhash';
-      $name = trim($name, "\"'");
-      return "<?php echo '<input type=\"hidden\" name=\"" . $name . "\" value=\"' . blockhash() . '\">'; ?>";
+    Blade::directive('blockhash', function () use ($mode) {
+      return "<?php echo \"<input type=\"hidden\" name=\"_blockhash\" value=\"" . blockhash() . "\">";
     });
   }
 }
