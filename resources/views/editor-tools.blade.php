9097df20909da598789ff32712a12b59513b487b
Thomas <thomas@alrek.no>
2021-12-22 15:19:49 +0000

    prepare release

--- resources/views/editor-tools.blade.php
+++ resources/views/editor-tools.blade.php
@@ -4,5 +4,5 @@
 @endmode
 <style>
 button, a, input[type="submit"] { pointer-events: none!important; }
-a.netflex-advanced-content-area, a.netflex-content-settings-btn { pointer-events: auto!important; }
+.find-image, .cke_button, .cke_button__link, .netflex-advanced-content-area, .netflex-content-settings-btn { pointer-events: auto!important; }
 </style>
