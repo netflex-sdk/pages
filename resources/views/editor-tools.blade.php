e5837f67f016a2ed4f6c19372ffe7f1fd87d06d5
Thomas <thomas@alrek.no>
2022-02-18 13:59:08 +0000

    prepare release

--- resources/views/editor-tools.blade.php
+++ resources/views/editor-tools.blade.php
@@ -1,8 +1,25 @@
-@mode('edit')
 {!! $editorTools !!}
 {{ $slot }}
-@endmode
 <style>
-button, a, input[type="submit"] { pointer-events: none!important; }
-.find-image, .find-image *, .cke, .cke *, .cke_reset_all, .cke_reset_all *, .netflex-advanced-content-area, .netflex-advanced-content-area *, .netflex-content-settings-btn, .netflex-content-settings-btn * { pointer-events: auto!important; }
+    button, a, input[type="submit"] { pointer-events: none!important; }
+    .find-image, .find-image *, .cke, .cke *, .cke_reset_all, .cke_reset_all *, .netflex-advanced-content-area, .netflex-advanced-content-area *, .netflex-content-settings-btn, .netflex-content-settings-btn * { pointer-events: auto!important; }
 </style>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        alert('hei')
+        document.querySelectorAll('form').forEach(function (form) {
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                e.preventBubbling();
+                return false;
+            });
+        });
+        document.querySelectorAll('button').forEach(function (form) {
+            form.addEventListener('click', function (e) {
+                e.preventDefault();
+                e.preventBubbling();
+                return false;
+            });
+        });
+    })
+</script>
