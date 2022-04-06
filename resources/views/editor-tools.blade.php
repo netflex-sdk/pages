a23af872759805e6b409b48e3f785712d2406972
Thomas <thomas@alrek.no>
2022-04-06 07:26:49 +0000

    prepare release

--- resources/views/editor-tools.blade.php
+++ resources/views/editor-tools.blade.php
@@ -1,7 +1,7 @@
 {!! $editorTools !!}
 {{ $slot }}
 <style>
-    button, a, input[type="submit"] { pointer-events: none!important; }
+    button[type="submit"], a[href^="http"], a[href^="/"], input[type="submit"] { pointer-events: none!important; }
     .find-image, .find-image *, .cke, .cke *, .cke_reset_all, .cke_reset_all *, .netflex-advanced-content-area, .netflex-advanced-content-area *, .netflex-content-settings-btn, .netflex-content-settings-btn * { pointer-events: auto!important; }
 </style>
 <script>
