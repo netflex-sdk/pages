70d84f11c87d2a5a0d11d91594da980204604637
Thomas <thomas@alrek.no>
2024-11-20 13:10:29 +0000

    refactor builder ES error handling

--- AbstractPage.php
+++ AbstractPage.php
@@ -196,7 +196,7 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    * @param mixed $key
    * @return array|null
    */
-  protected function performRetrieveRequest(?int $relationId = null, $key)
+  protected function performRetrieveRequest(?int $relationId = null, mixed $key = null)
   {
     return API::get('builder/pages/' . $key, true);
   }
@@ -223,7 +223,7 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    * @param array $attributes
    * @return void
    */
-  protected function performUpdateRequest(?int $relationId = null, $key, $attributes = [])
+  protected function performUpdateRequest(?int $relationId = null, mixed $key = null, array $attributes = [])
   {
     return API::put('builder/pages/' . $key, $attributes);
   }
@@ -235,7 +235,7 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    * @param mixed $key
    * @return bool
    */
-  protected function performDeleteRequest(?int $relationId = null, $key)
+  protected function performDeleteRequest(?int $relationId = null, mixed $key = null)
   {
     return !!API::delete('builder/pages/' . $key);
   }
@@ -422,12 +422,9 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    * Retrieves all instances
    *
    * @return Collection|LazyCollection
-   * @throws NotQueryableException If object not queryable
-   * @throws QueryException On invalid query
    */
   public static function all()
   {
-
     return static::getPages()->values();
   }
 
@@ -436,8 +433,6 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    *
    * @param int|string $id
    * @return static|null
-   * @throws NotQueryableException If object not queryable
-   * @throws QueryException On invalid query
    */
   public static function find($id)
   {
@@ -451,8 +446,6 @@ abstract class AbstractPage extends QueryableModel implements Responsable
    * @param mixed $resolveBy
    * @param string|null $field
    * @return static|Collection|null
-   * @throws NotQueryableException If object not queryable
-   * @throws QueryException On invalid query
    */
   public static function resolve($resolveBy, $field = null)
   {
