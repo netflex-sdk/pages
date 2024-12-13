4ca186e368c0152dbdae1ac73ef9c60a8b240788
Thomas <thomas@alrek.no>
2024-12-13 13:49:45 +0000

    upgrade spatie/once to 3.1

--- NavigationData.php
+++ NavigationData.php
@@ -30,6 +30,7 @@ class NavigationData implements JsonSerializable
     $this->attributes = $attributes;
   }
 
+  #[\ReturnTypeWillChange]
   public function jsonSerialize()
   {
     return $this->__debugInfo();
